b5f4cd1548753dc991c6dbd78cb3e4ff6b19d334
Batıkan Senemoğlu <batikan17@gmail.com>
2017-10-09 10:52:26 +0300

    Update ViewStore.js
    Corrected wrong spaced assignment operator.

--- src/stores/ViewStore.js
+++ src/stores/ViewStore.js
@@ -3,5 +3,5 @@ import { ALL_TODOS } from '../constants';
 
 export default class ViewStore {
 	@observable todoBeingEdited = null;
-	@observable todoFilter= ALL_TODOS;
+	@observable todoFilter = ALL_TODOS;
 }
